e201ba88f2fb0cb5ebe5006f4c2f22b6bee29123
ashraful-MechE <ashraful.islam.009@gmail.com>
2014-01-20 17:54:20 +0600

    Dano demo theme
    Charlotte demo theme for populr.me

---
+++ js/main.js
@@ -0,0 +1,9 @@
+/************************************************************************
+// For extensive details about creating a Populr theme, please visit
+// http://developers.populr.me/theming
+*/
+
+
+// $(document).on('pop-initialized', function(){
+//
+// });
